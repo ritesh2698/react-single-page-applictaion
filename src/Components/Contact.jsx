eb58c238b285ceca378f8b9e58d88897f52d214f
ritesh prajapati <riteshprajapati30@gmail.com>
2021-10-15 19:41:25 +0530

    first commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -16,6 +16,7 @@ const Contact = () => {
             </div>
             <div className="iframe">
                 <iframe
+                title="hero"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29388.344740030763!2d72.61133193955078!3d22.967048399999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e899ed07dad55%3A0xc9e54ada874e72a8!2sR.%20N.%20INDUSTRIES!5e0!3m2!1sen!2sin!4v1634284034110!5m2!1sen!2sin" 
                     // width="600" 
                     // height="450" 
